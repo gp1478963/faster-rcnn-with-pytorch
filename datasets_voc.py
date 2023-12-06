a82d6d9926626429159da1e46467d744abecae31
My Name <my-name@chromium.org>
2023-12-06 17:02:16 +0800

    add

--- datasets_voc.py
+++ datasets_voc.py
@@ -4,84 +4,96 @@ import numpy
 import torch
 import torchvision
 import torchvision.transforms.v2 as v2
-from PIL.Image import Image
 import xml.etree.ElementTree as ET
+from PIL import Image
 classicers = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
               "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
 
+
 class BoxScale:
+    def __init__(self, adjust_post_h=448, adjust_post_w=448):
+        self.adjust_post_h = adjust_post_h
+        self.adjust_post_w = adjust_post_w
+
     def __call__(self, *args, **kwargs):
-        pass
+        image = args[0]
+        boxes = args[1]
+        labels = args[2]
 
+        image_orig_h, image_orig_w = image.size
+        scale_h = self.adjust_post_h / image_orig_h
+        scale_w = self.adjust_post_w / image_orig_w
 
-voc_dataset_image_transform = v2.Compose([
-    v2.ToImageTensor(),
-    v2.ConvertImageDtype(),
-    v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    v2.Resize((224, 224))
-])
+        boxes[:, [0, 2]] = boxes[:, [0, 2]] * scale_w
+        boxes[:, [1, 3]] = boxes[:, [1, 3]] * scale_h
 
-voc_trainval_dataset = torchvision.datasets.VOCDetection(root='D:\\image\\datasets\\VOC2007\\VOCtrainval_06-Nov-2007',
-                                                         year='2007', image_set='train', download=False)
-voc_train_val_dataset_loader = torch.utils.data.DataLoader(voc_trainval_dataset, batch_size=1, shuffle=True)
+        image = v2.Resize((224, 224))(image)
 
-image, target = voc_trainval_dataset.__getitem__(1)
+        return (image, boxes, labels) + args[3:]
 
 
 class VOCDetectionD(torchvision.datasets.VOCDetection):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
     def __getitem__(self, index: int) -> Tuple[Any, Any]:
         img = Image.open(self.images[index]).convert("RGB")
-        target = self.parse_voc_xml(ET.ET_parse(self.annotations[index]).getroot())
-
+        target_ = self.parse_voc_xml(ET.parse(self.annotations[index]).getroot())
+        boxes_, labels_ = self._boxes_and_labels_adjust(target_)
+        img, boxes_, labels_ = BoxScale(224, 224)(img, boxes_, labels_)
         if self.transforms is not None:
-            img, target = self.transforms(img, target)
+            img = self.transforms(img)
 
-        return img, target
+        target_ = {}
+        target_['boxes'] = boxes_
+        target_['labels'] = labels_
+        return img, [target_]
+
+    def _boxes_and_labels_adjust(self, target):
+        boxes_ = []
+        labels_ = []
+
+        for image_target_t in target['annotation']['object']:
+            x1_, y1_, x2_, y2_ = (int(image_target_t['bndbox']['xmin']), int(image_target_t['bndbox']['ymin']),
+                                  int(image_target_t['bndbox']['xmax']), int(image_target_t['bndbox']['ymax']))
+            label = classicers.index(image_target_t['name'])
+
+            boxes_.insert(-1, [x1_, y1_, x2_, y2_])
+            labels_.insert(-1, label)
+
+        boxes_ = torch.from_numpy(numpy.array(boxes_)).float()
+        labels_ = torch.from_numpy(numpy.array(labels_, dtype=numpy.int64))
+        return boxes_, labels_
 
 
 if __name__ == '__main__':
     import predict_image_box
-    (image_size_original_h, image_size_original_w) = image.size
-    # image size调整到（224， 224）尺寸下需要缩放的倍数，box一样需要调整
-    scale_h, scale_w = 224 / image_size_original_h, 224 / image_size_original_w
-    image1, target = voc_dataset_image_transform(image, target)
-    image1 = torch.unsqueeze(image1, dim=0)
 
-    boxes = []
-    labels = []
-    for image_target in target['annotation']['object']:
-        x1, y1, x2, y2 = (int(image_target['bndbox']['xmin']), int(image_target['bndbox']['ymin']),
-                          int(image_target['bndbox']['xmax']), int(image_target['bndbox']['ymax']))
-        label = classicers.index(image_target['name'])
+    voc_dataset_image_transform = v2.Compose([
+        BoxScale(224, 224),
+        v2.ToImageTensor(),
+        v2.ConvertImageDtype(),
+        v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    voc_trainval_dataset = VOCDetectionD(root='D:\\image\\datasets\\VOC2007\\VOCtrainval_06-Nov-2007',
+                                         year='2007', image_set='train', download=False,
+                                         transforms=voc_dataset_image_transform)
+    voc_train_val_dataset_loader = torch.utils.data.DataLoader(voc_trainval_dataset, batch_size=1, shuffle=True)
 
-        boxes.insert(-1, [x1, y1, x2, y2])
-        labels.insert(-1, label)
+    (image, target) = voc_trainval_dataset.__getitem__(1)
+    image = torch.unsqueeze(image, dim=0)
 
-    boxes = torch.from_numpy(numpy.array(boxes)).float()
-    labels = torch.from_numpy(numpy.array(labels, dtype=numpy.int64))
-
-    boxes[:, [0, 2]] = boxes[:, [0, 2]] * scale_h
-    boxes[:, [1, 3]] = boxes[:, [1, 3]] * scale_w
-    target = []
-    target.insert(-1, {'boxes': boxes, 'labels': labels})
-    # 至此，标签制作完成
     predict_image_box.faster_rcnn_frame.train()
+    loss = predict_image_box.faster_rcnn_frame(image, target)
+    loss_classifier = loss['loss_classifier']
+    loss_box_reg = loss['loss_box_reg']
+    loss_objectness = loss['loss_objectness']
+    loss_rpn_box_reg = loss['loss_rpn_box_reg']
 
-    loss_classifier,  loss_box_reg, loss_objectness, loss_rpn_box_reg = predict_image_box.faster_rcnn_frame(image1, target)
+
+    # print(loss_classifier, '\t', loss_box_reg, '\t', loss_objectness, '\t', loss_rpn_box_reg)
     # predictions_boxs, predictions_labels, predictions_scores = predictons['boxes'], predictons['labels'], predictons['scores'],
     # keep_index = torchvision.ops.nms(predictions_boxs, predictions_scores, iou_threshold=0.1)
     # print(predictions_boxs[keep_index])
-    # print(predictions_labels[keep_index])
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # # print(predictions_labels[keep_index])
---
+++ train.py
@@ -0,0 +1,37 @@
+import datasets_voc
+import predict_image_box
+import torch
+import torchvision
+import os
+import torchvision.transforms.v2 as v2
+
+BATCH_SIZE = 1
+EPOCH = 1
+
+if __name__ == '__main__':
+    voc_dataset_image_transform = v2.Compose([
+        # datasets_voc.BoxScale(224, 224),
+        v2.ToImageTensor(),
+        v2.ConvertImageDtype(),
+        v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    voc_trainval_dataset = datasets_voc.VOCDetectionD(root='D:\\image\\datasets\\VOC2007\\VOCtrainval_06-Nov-2007',
+                                                      year='2007', image_set='train', download=False,
+                                                      transforms=voc_dataset_image_transform)
+    voc_train_val_dataset_loader = torch.utils.data.DataLoader(dataset=voc_trainval_dataset, batch_size=4, shuffle=True)
+
+    for epoch in range(EPOCH):
+        for (image, target) in voc_train_val_dataset_loader:
+            # for t in target:
+            #     t['boxes'] = torch.squeeze(t['boxes'], 0)
+            #     t['labels'] = torch.squeeze(t['labels'], 0)
+
+            predict_image_box.faster_rcnn_frame.train()
+            loss = predict_image_box.faster_rcnn_frame(image, target)
+
+            loss_classifier = loss['loss_classifier']
+            loss_box_reg = loss['loss_box_reg']
+            loss_objectness = loss['loss_objectness']
+            loss_rpn_box_reg = loss['loss_rpn_box_reg']
+
+            print(loss_classifier, '\t', loss_box_reg, '\t', loss_objectness, '\t', loss_rpn_box_reg)
