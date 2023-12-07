06cfcffe99745cc549e58b20855dcfd89d199458
My Name <my-name@chromium.org>
2023-12-07 16:21:47 +0800

    add sgd optimizer

--- predict_image_box.py
+++ predict_image_box.py
@@ -1,40 +1,38 @@
 import torch
-import numpy
 import torchvision as vision
-import torchvision.transforms.v2 as v2
-import cv2
+
+def backbone_resnet50_layer(freeze=False):
+    model_resudal_neutral_50_layer = vision.models.resnet50(weights=vision.models.ResNet50_Weights.IMAGENET1K_V1)
+    backbone_resudal_neutral_50_layer = torch.nn.Sequential(
+        model_resudal_neutral_50_layer.conv1,
+        model_resudal_neutral_50_layer.bn1,
+        model_resudal_neutral_50_layer.relu,
+        model_resudal_neutral_50_layer.maxpool,
+        model_resudal_neutral_50_layer.layer1,
+        model_resudal_neutral_50_layer.layer2,
+        model_resudal_neutral_50_layer.layer3,
+        model_resudal_neutral_50_layer.layer4,
+        model_resudal_neutral_50_layer.avgpool,
+    )
+    backbone_resudal_neutral_50_layer.out_channels = 2048
+
+    if freeze:
+        for param in backbone_resudal_neutral_50_layer.parameters():
+            param.requires_grad_(False)
+    return backbone_resudal_neutral_50_layer
 
 
-model_resudal_neutral_50_layer = vision.models.resnet50(weights=vision.models.ResNet50_Weights.IMAGENET1K_V1)
-backbone_resudal_neutral_50_layer = torch.nn.Sequential(
-            model_resudal_neutral_50_layer.conv1,
-            model_resudal_neutral_50_layer.bn1,
-            model_resudal_neutral_50_layer.relu,
-            model_resudal_neutral_50_layer.maxpool,
-            model_resudal_neutral_50_layer.layer1,
-            model_resudal_neutral_50_layer.layer2,
-            model_resudal_neutral_50_layer.layer3,
-            model_resudal_neutral_50_layer.layer4,
-            model_resudal_neutral_50_layer.avgpool,
-)
-backbone_resudal_neutral_50_layer.out_channels = 2048
+def generate_faster_rcnn_model(freeze=False):
+    backbone = backbone_resnet50_layer(freeze=freeze)
+    anchor_generator = vision.models.detection.rpn.AnchorGenerator(sizes=((32, 64, 128, 256, 512),),
+                                                                   aspect_ratios=((0.5, 1.0, 2.0),))
+    roi_pooler = vision.ops.MultiScaleRoIAlign(featmap_names=['0'], output_size=7, sampling_ratio=2)
+    faster_rcnn_frame = vision.models.detection.FasterRCNN(
+        backbone=backbone,
+        num_classes=20,
+        rpn_anchor_generator=anchor_generator,
+        box_roi_pool=roi_pooler
+    )
 
-anchor_generator = vision.models.detection.rpn.AnchorGenerator(sizes=((32, 64, 128, 256, 512),),
-                                  aspect_ratios=((0.5, 1.0, 2.0),))
-
-roi_pooler = vision.ops.MultiScaleRoIAlign(featmap_names=['0'], output_size=7, sampling_ratio=2)
-
-faster_rcnn_frame = vision.models.detection.FasterRCNN(
-    backbone=backbone_resudal_neutral_50_layer, 
-    num_classes=20,
-    rpn_anchor_generator=anchor_generator,
-    box_roi_pool=roi_pooler
-)
-
-faster_rcnn_frame.eval()
-# x = [torch.rand(3, 300, 400), torch.rand(3, 500, 400)]
-#
-# predictions = faster_rcnn_frame(x)
-# print(predictions)
-
+    return faster_rcnn_frame
 
--- train.py
+++ train.py
@@ -6,7 +6,31 @@ import os
 import torchvision.transforms.v2 as v2
 
 BATCH_SIZE = 1
-EPOCH = 1
+EPOCH = 10
+
+
+def train(model, train_loader, val_loader, optimizer, criterion, lr_scheduler, device, epoch_count, epoch_total):
+    model.to(device)
+    losses = 0.0
+    model.train()
+    for percent, (image, target) in enumerate(train_loader):
+        loss = model(image, target)
+        loss_classifier = loss['loss_classifier']
+        loss_box_reg = loss['loss_box_reg']
+        loss_objectness = loss['loss_objectness']
+        loss_rpn_box_reg = loss['loss_rpn_box_reg']
+        loss_total = loss_classifier + loss_box_reg + loss_objectness + loss_rpn_box_reg
+        optimizer.zero_grad()
+        loss_total.backward()
+        optimizer.step()
+        lr_scheduler.step()
+        losses += loss_total
+        average_loss = losses / percent
+        print('epoch:<{0}/{1}>, batch:<{2}/{3}>, loss:[{4:.4f}], average loss:[{5:.4f}]'.format(
+            epoch_count, epoch_total, percent, train_loader.dataset.__len__(), loss_total.item(), average_loss))
+
+    torch.save(model.state_dict(), './out/model_{0}.pth'.format(epoch_count))
+
 
 if __name__ == '__main__':
     voc_dataset_image_transform = v2.Compose([
@@ -18,18 +42,16 @@ if __name__ == '__main__':
     voc_trainval_dataset = datasets_voc.VOCDetectionD(root='D:\\image\\datasets\\VOC2007\\VOCtrainval_06-Nov-2007',
                                                       year='2007', image_set='train', download=False,
                                                       transforms=voc_dataset_image_transform)
-    voc_train_val_dataset_loader = torch.utils.data.DataLoader(dataset=voc_trainval_dataset, batch_size=2, shuffle=True,
-                                                               collate_fn=datasets_voc.VocDatasetCollater())
+    voc_train_val_dataset_loader = torch.utils.data.DataLoader(dataset=voc_trainval_dataset, batch_size=BATCH_SIZE,
+                                                               shuffle=True, collate_fn=datasets_voc.VocDatasetCollater())
+
+    model = predict_image_box.generate_faster_rcnn_model(freeze=True)
+
+    params = [param for param in model.parameters() if param.requires_grad]
+    sgd_optimizer = torch.optim.SGD(params=params, lr=0.005, momentum=0.9, weight_decay=0.0005)
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer=sgd_optimizer, step_size=3, )
 
     for epoch in range(EPOCH):
-        for (image, target) in voc_train_val_dataset_loader:
-
-            predict_image_box.faster_rcnn_frame.train()
-            loss = predict_image_box.faster_rcnn_frame(image, target)
-
-            loss_classifier = loss['loss_classifier']
-            loss_box_reg = loss['loss_box_reg']
-            loss_objectness = loss['loss_objectness']
-            loss_rpn_box_reg = loss['loss_rpn_box_reg']
-
-            print(loss_classifier, '\t', loss_box_reg, '\t', loss_objectness, '\t', loss_rpn_box_reg)
+        train(model=model, train_loader=voc_train_val_dataset_loader,
+              val_loader=None, optimizer=sgd_optimizer, criterion=None,
+              lr_scheduler=lr_scheduler, device='cpu', epoch_count=epoch+1, epoch_total=EPOCH)
